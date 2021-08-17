250400268c677f586bdd41fc715fca4aa3fc3710
rafael boas <rafaelrvb@gmail.com>
2021-08-17 14:16:16 -0300

    done with action: show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,9 @@
 class TasksController < ApplicationController
+  def index
+    @index_tasks = Task.all # assigning all tasks from DB tasks to instance var all_tasks
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
