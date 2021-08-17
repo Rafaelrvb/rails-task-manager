959680305f706f22d34055cc49c61be18c49d242
rafael boas <rafaelrvb@gmail.com>
2021-08-17 17:01:30 -0300

    first one done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,5 +12,31 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+     raise
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+
+  end
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
